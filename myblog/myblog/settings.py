f423f6af7d40a0886fd3cc7b87304b8c6887e530
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-17 21:02:47 +0530

    hi

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
